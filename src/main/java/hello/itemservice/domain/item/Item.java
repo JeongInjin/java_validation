605ec1eb33ab6033d325e725878a1a65855be35e
injinjeong <snowwwwww@naver.com>
2022-03-01 22:14:16 +0900

    add

--- src/main/java/hello/itemservice/domain/item/Item.java
+++ src/main/java/hello/itemservice/domain/item/Item.java
@@ -2,12 +2,25 @@ package hello.itemservice.domain.item;
 
 import lombok.Data;
 
+import javax.validation.constraints.Max;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+
 @Data
 public class Item {
 
+    //    @NotNull(groups = UpdateCheck.class) //수정 요구사항 추가
+    @NotNull
     private Long id;
+
+    //    @NotBlank(groups = {SaveCheck.class, UpdateCheck.class})
+    @NotBlank
     private String itemName;
+
+    @NotNull
     private Integer price;
+
+    @Max(value = 9999)
     private Integer quantity;
 
     public Item() {
